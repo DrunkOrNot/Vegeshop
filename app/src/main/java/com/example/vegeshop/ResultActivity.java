81a8c157608f391240266340b1c2fbfe9d75b90c
KeyLook <zachara_damian@wp.pl>
2020-10-29 20:00:14 +0100

    Added missing Result Activity

---
+++ app/src/main/java/com/example/vegeshop/ResultActivity.java
@@ -0,0 +1,36 @@
+package com.example.vegeshop;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    Button btnForceAdd;
+    TextView txtID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        btnForceAdd.findViewById(R.id.btnForceAdd);
+        txtID.findViewById(R.id.txtID);
+
+
+
+        btnForceAdd.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                ResultActivity.this.startActivity(new Intent(ResultActivity.this, AddProductActivity.class));
+            }
+        });
+
+    }
+}
