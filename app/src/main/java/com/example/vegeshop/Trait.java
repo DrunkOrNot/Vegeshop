206cd5fe4ddb97964c481e627f20901b5bdcbeea
Dominika <ddd.wilk@gmail.com>
2020-11-29 17:17:33 +0100

    Reworking Trait to StringDef WIP

--- app/src/main/java/com/example/vegeshop/Trait.java
+++ app/src/main/java/com/example/vegeshop/Trait.java
@@ -1,11 +1,42 @@
 package com.example.vegeshop;
 
-public enum Trait {
-    Meat,
-    Diary,
-    Gluten,
-    Honey,
-    Fish,
-    Seafood,
-    Lactose,
+import androidx.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public class Trait {
+
+    @Retention(RetentionPolicy.SOURCE)
+    @StringDef({
+            MEAT,
+            DIARY,
+            GLUTEN,
+            HONEY,
+            FISH,
+            SEAFOOD,
+            LACTOSE,
+    })
+
+    @interface IngredientTrait{}
+
+public static final String MEAT = "Meat";
+public static final String DIARY = "Diary";
+public static final String GLUTEN = "Gluten";
+public static final String HONEY = "Honey";
+public static final String FISH = "Fish";
+public static final String SEAFOOD = "Seafood";
+public static final String LACTOSE = "Lactose";
+
+private String trait;
+
+public void setTrait(@IngredientTrait String trait) {
+this.trait = trait;
 }
+
+    @IngredientTrait
+    public String getTrait() {
+           return trait;
+        }
+}
+       // https://www.youtube.com/watch?v=EP3XqzvAgts
