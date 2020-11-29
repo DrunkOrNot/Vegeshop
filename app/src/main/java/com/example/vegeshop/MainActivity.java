f42a36c23fe1ffc3c6aa5366a970fdd4b1b7ca51
KeyLook <zachara_damian@wp.pl>
2020-11-29 19:51:20 +0100

    Adding splash activity

--- app/src/main/java/com/example/vegeshop/MainActivity.java
+++ app/src/main/java/com/example/vegeshop/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.vegeshop;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainActivity.this.startActivity(new Intent(MainActivity.this, CameraActivity.class));
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageView.startAnimation(animation);
+
+        Thread timer = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(3500);
+                     Intent intentMain = new Intent(getApplicationContext(), CameraActivity.class);
+                    startActivity(intentMain);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.start();
     }
 }
